b48b712a8fad654ef23beb2c0cb8e3adef86d576
sarvan.kattamuru <sarvan.kattamuru@thoughtworks.com>
2023-08-03 23:34:15 +0530

    Adds base build files | Sarvan9

---
+++ src/main/java/com/spring/kafka/steams/SteamsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kafka.steams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/kafka/steams/SteamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.kafka.steams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
